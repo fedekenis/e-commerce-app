4b2e8535d5e5491fb53d693aed3fa9a12301d0fc
fedekenis <fedekenis@gmail.com>
2022-04-02 15:11:46 -0300

    install mui and create card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Product from './components/Product';
 
 function App() {
   return (
     <div className="App">
       <h1>React</h1>
+      <Product/>
     </div>
   );
 }
---
+++ src/components/Product.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { AddShoppingCart } from '@material-ui/icons';
+import accounting from "accounting"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  action: {
+      marginTop: "1rem"
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+}));
+
+export default function Product() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        action={
+         <Typography
+         className={classes.action}
+         variant='h5'
+         color='textSecondary'>
+             {accounting.formatMoney(50, '$')}
+        </Typography>
+        }
+        title="Shoes"
+        subheader="in Stock"
+      />
+      <CardMedia
+        className={classes.media}
+        image="https://s2.r29static.com/bin/entry/ebd/0,675,2000,1050/x,80/1929471/iamge.jpg"
+        title="Nike shoes"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Running Shoes
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCart fontSize='large'/>
+        </IconButton>
+        {Array(4)
+        .fill()
+        .map((_,i) => (
+            <p>&#11088;</p>
+        ))
+        }
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+          Zapatillas para correr
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
